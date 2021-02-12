163f698389087a315fb58bd58b7502a2d2e7c078
Benjamin Russell <benrr101@outlook.com>
2021-02-12 02:36:03 +0000

    AIFF support (#22)
    * Adding AIFF stream header
    
    * AIFF file implementation and beginning of integration test
    It don't work :(
    
    * Adding more aiff tests, fixing a big bug in aiff file detection
    
    * More tests working :grimace:
    
    * WIP for fixing bug with writing smaller file contents
    
    * Fixed!
    
    * WIP to do proper calculation of sample rate
    
    * Finishing unit tests for AIFF for now

---
+++ test-integration/aiff_fileTests.ts
@@ -0,0 +1,82 @@
+import * as Chai from "chai";
+import TestConstants from "./utilities/testConstants";
+import {suite, test} from "mocha-typescript";
+
+import {File, ReadStyle} from "../src";
+import {StandardFileTests} from "./utilities/standardFileTests";
+import ExtendedFileTests from "./utilities/extendedFileTests";
+
+const assert = Chai.assert;
+
+@suite class Aiff_FileTests {
+    private static readonly corruptFilePath = TestConstants.getCorruptFilePath("corrupt.aif");
+    private static readonly sampleFilePath = TestConstants.getSampleFilePath("sample.aif");
+    private static readonly tmpFileName = "tmpwrite.aif";
+
+    private static file: File;
+
+    public static before() {
+        Aiff_FileTests.file = File.createFromPath(Aiff_FileTests.sampleFilePath);
+    }
+
+    public static after() {
+        Aiff_FileTests.file.dispose();
+    }
+
+    @test
+    public readAudioProperties() {
+        assert.approximately(Aiff_FileTests.file.properties.audioBitrate, 706, 0.5);
+        assert.strictEqual(Aiff_FileTests.file.properties.audioChannels, 1);
+        assert.strictEqual(Aiff_FileTests.file.properties.audioSampleRate, 44100);
+        assert.strictEqual(Aiff_FileTests.file.properties.bitsPerSample, 16);
+        assert.approximately(Aiff_FileTests.file.properties.durationMilliseconds, 2000, 1);
+    }
+
+    @test
+    public readTags() {
+        assert.strictEqual(Aiff_FileTests.file.tag.album, "Aiff Album");
+        assert.strictEqual(Aiff_FileTests.file.tag.firstPerformer, "Aiff Artist");
+        assert.strictEqual(Aiff_FileTests.file.tag.comment, "Aiff Comment");
+        assert.strictEqual(Aiff_FileTests.file.tag.firstGenre, "Blues");
+        assert.strictEqual(Aiff_FileTests.file.tag.track, 5);
+        assert.strictEqual(Aiff_FileTests.file.tag.trackCount, 10);
+
+        // sample.aif contains a TDAT (and no TYER) with 2009 in it, but TDAT is supposed to
+        // contain MMDD - so the following should not be equal
+        assert.notStrictEqual(2009, Aiff_FileTests.file.tag.year);
+    }
+
+    @test
+    public testCorruptionResistance() {
+        StandardFileTests.testCorruptionResistance(Aiff_FileTests.corruptFilePath);
+    }
+
+    @test
+    public writeExtendedTags() {
+        ExtendedFileTests.writeExtendedTags(Aiff_FileTests.sampleFilePath, Aiff_FileTests.tmpFileName);
+    }
+
+    @test
+    public writeStandardPictures() {
+        StandardFileTests.writeStandardPictures(
+            Aiff_FileTests.sampleFilePath,
+            Aiff_FileTests.tmpFileName,
+            ReadStyle.None
+        );
+    }
+
+    // @test
+    // public writeStandardPicturesLazy() {
+    //     StandardFileTests.writeStandardPictures(
+    //         Aiff_FileTests.sampleFilePath,
+    //         Aiff_FileTests.tmpFileName,
+    //         ReadStyle.None
+    //     );
+    // }
+
+    @test
+    public writeStandardTags() {
+        StandardFileTests.writeStandardTags(Aiff_FileTests.sampleFilePath, Aiff_FileTests.tmpFileName);
+    }
+
+}
---
+++ test-unit/aiff/aiffStreamHeaderTests.ts
@@ -0,0 +1,112 @@
+import * as Chai from "chai";
+import Testers from "../utilities/testers";
+import {suite, test} from "mocha-typescript";
+
+import AiffStreamHeader from "../../src/aiff/aiffStreamHeader";
+import {ByteVector} from "../../src/byteVector";
+import {MediaTypes} from "../../src/iCodec";
+
+// Setup Chai
+const assert = Chai.assert;
+
+@suite class Aiff_StreamHeaderTests {
+    @test
+    public constructor_invalidData() {
+        // Arrange
+        const testFunc = (a: ByteVector, b: number) => { const _ = new AiffStreamHeader(a, b); };
+
+        // Act / Asseert
+        Testers.testTruthy((v: ByteVector) => testFunc(v, 0));
+        Testers.testUint((v: number) => testFunc(ByteVector.empty(), v));
+    }
+
+    @test
+    public constructor_dataDoesNotStartWithIdentifier() {
+        // Act / Assert
+        assert.throws(() => { const _ = new AiffStreamHeader(ByteVector.fromSize(10), 0); });
+    }
+
+    @test
+    public constructor_validData() {
+        // Arrange
+        const data = ByteVector.concatenate(
+            ByteVector.fromString("COMM"), // Chunk identifier
+            ByteVector.fromUInt(18), // Chunk size
+            ByteVector.fromUShort(1234), // Channels
+            ByteVector.fromUInt(2345), // Total frames
+            ByteVector.fromUShort(3456), // Bits per sample
+            // Sample rate (2345678)
+            ByteVector.fromByteArray(new Uint8Array([0x40, 0x14, 0x8F, 0x2B, 0x38, 0x00, 0x00, 0x00, 0x00, 0x00]))
+        );
+
+        // Act
+        const header = new AiffStreamHeader(data, 1024);
+
+        // Assert
+        assert.isOk(header);
+        assert.strictEqual(header.mediaTypes, MediaTypes.Audio);
+        assert.strictEqual(header.audioChannels, 1234);
+        assert.strictEqual(header.audioSampleRate, 2345678);
+        assert.strictEqual(header.bitsPerSample, 3456);
+        assert.strictEqual(header.description, "AIFF Audio");
+
+        assert.approximately(header.durationMilliseconds, 0.9997, 0.0001);
+        assert.approximately(header.audioBitrate, 8194, 1);
+    }
+
+    @test
+    public constructor_zeroSampleRate() {
+        // Arrange
+        const data = ByteVector.concatenate(
+            ByteVector.fromString("COMM"), // Chunk identifier
+            ByteVector.fromUInt(18), // Chunk size
+            ByteVector.fromUShort(1234), // Channels
+            ByteVector.fromUInt(2345), // Total frames
+            ByteVector.fromUShort(3456), // Bits per sample
+            // Sample rate (0)
+            ByteVector.fromByteArray(new Uint8Array([0x3C, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]))
+        );
+
+        // Act
+        const header = new AiffStreamHeader(data, 1024);
+
+        // Assert
+        assert.isOk(header);
+        assert.strictEqual(header.mediaTypes, MediaTypes.Audio);
+        assert.strictEqual(header.audioChannels, 1234);
+        assert.strictEqual(header.audioSampleRate, 0);
+        assert.strictEqual(header.bitsPerSample, 3456);
+        assert.strictEqual(header.description, "AIFF Audio");
+
+        assert.strictEqual(header.durationMilliseconds, 0);
+        assert.strictEqual(header.audioBitrate, 0);
+    }
+
+    @test
+    public constructor_zeroTotalFrames() {
+        // Arrange
+        const data = ByteVector.concatenate(
+            ByteVector.fromString("COMM"), // Chunk identifier
+            ByteVector.fromUInt(18), // Chunk size
+            ByteVector.fromUShort(1234), // Channels
+            ByteVector.fromUInt(0), // Total frames
+            ByteVector.fromUShort(3456), // Bits per sample
+            // Sample rate
+            ByteVector.fromByteArray(new Uint8Array([0x40, 0x14, 0x8F, 0x2B, 0x38, 0x00, 0x00, 0x00, 0x00, 0x00]))
+        );
+
+        // Act
+        const header = new AiffStreamHeader(data, 1024);
+
+        // Assert
+        assert.isOk(header);
+        assert.strictEqual(header.mediaTypes, MediaTypes.Audio);
+        assert.strictEqual(header.audioChannels, 1234);
+        assert.strictEqual(header.audioSampleRate, 2345678);
+        assert.strictEqual(header.bitsPerSample, 3456);
+        assert.strictEqual(header.description, "AIFF Audio");
+
+        assert.strictEqual(header.durationMilliseconds, 0);
+        assert.strictEqual(header.audioBitrate, 0);
+    }
+}
