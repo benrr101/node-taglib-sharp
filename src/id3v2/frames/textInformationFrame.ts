b0407504cd5175388d392c7b1cbfa402e06b7c29
Digimezzo <digimezzo@outlook.com>
2025-12-13 14:47:54 +0100

    Fixes #120: ID3v2.3 genres are trimmed

--- src/id3v2/frames/textInformationFrame.ts
+++ src/id3v2/frames/textInformationFrame.ts
@@ -459,7 +459,7 @@ export class TextInformationFrame extends Frame {
                 if (value.length > 0) {
                     // Split the remaining genre value by dividers if the setting is turned on
                     let splitValue = Id3v2Settings.useNonStandardV2V3GenreSeparators
-                        ? value.split(/[\/;]/).map((v) => v.trim()).filter((v) => !!v)
+                        ? value.split(/[\/;]/).filter((v) => !!v)
                         : [value];
 
                     splitValue = splitValue.map((v) => {
