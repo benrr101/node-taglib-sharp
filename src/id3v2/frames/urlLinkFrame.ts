ee543a0d4c960959c250ce0f4af7cbcd667c0ae8
Ben Russell <ben@holysmokesitsbenrussell.com>
2026-05-11 23:49:27 -0500

    :robot: Remove UrlLinkFrame.fromRawData()

--- src/id3v2/frames/urlLinkFrame.ts
+++ src/id3v2/frames/urlLinkFrame.ts
@@ -94,21 +94,6 @@ export class UrlLinkFrame extends Frame {
         return frame;
     }
 
-    /**
-     * Constructs and initializes a new instance by reading its raw data in a specified
-     * ID3v2 version.
-     * @param data Raw representation of the new frame
-     * @param version ID3v2 version the raw frame is encoded with, must be a positive 8-bit integer
-     */
-    public static fromRawData(data: ByteVector, version: number): UrlLinkFrame {
-        Guards.truthy(data, "data");
-        Guards.byte(version, "version");
-
-        const frame = new UrlLinkFrame(Id3v2FrameHeader.fromData(data, version));
-        frame.setData(data, 0, true, version);
-        return frame;
-    }
-
     // #endregion
 
     // #region Properties
--- test-unit/id3v2/urlLinkFrameTests.ts
+++ test-unit/id3v2/urlLinkFrameTests.ts
@@ -25,8 +25,10 @@ const getTestFrameData = (): ByteVector => {
 };
 
 const getTestUrlLinkFrame = (): UrlLinkFrame => {
+    const header = new Id3v2FrameHeader(FrameIdentifiers.WXXX);
+    header.frameSize = 8;
     const frameData = getTestFrameData();
-    return UrlLinkFrame.fromRawData(frameData, 4);
+    return UrlLinkFrame.fromOffsetRawData(frameData, 0, header, 4);
 };
 
 @suite class Id3v2_UrlLinkFrame_ConstructorTests extends FrameConstructorTests {
@@ -34,9 +36,7 @@ const getTestUrlLinkFrame = (): UrlLinkFrame => {
         return UrlLinkFrame.fromOffsetRawData;
     }
 
-    public get fromRawData(): (d: ByteVector, v: number) => Frame {
-        return UrlLinkFrame.fromRawData;
-    }
+    public get fromRawData(): (d: ByteVector, v: number) => Frame { return undefined; }
 
     @test
     public fromIdentity_falsyIdentity() {
@@ -88,50 +88,6 @@ const getTestUrlLinkFrame = (): UrlLinkFrame => {
 
         // Act
         const output = UrlLinkFrame.fromOffsetRawData(data, 2, header, 4);
-
-        // Assert
-        Id3v2_UrlLinkFrame_ConstructorTests.assertFrame(
-            output,
-            FrameIdentifiers.WXXX,
-            ["foo", "bar"],
-            StringType.Latin1
-        );
-    }
-
-    @test
-    public fromRawData_notUserFrame() {
-        // Arrange
-        const header = new Id3v2FrameHeader(FrameIdentifiers.WCOM);
-        header.frameSize = 8;
-        const data = ByteVector.concatenate(
-            header.render(4),
-            ByteVector.fromString("foobar", StringType.Latin1),    // - Data
-            0x00, 0x00                                             // - null bytes to test end byte
-                                                                   //   cleanup
-        );
-
-        // Act
-        const output = UrlLinkFrame.fromRawData(data, 4);
-
-        // Assert
-        Id3v2_UrlLinkFrame_ConstructorTests.assertFrame(output, FrameIdentifiers.WCOM, ["foobar"], StringType.Latin1);
-    }
-
-    @test
-    public fromRawData_userFrame() {
-        // Arrange
-        const header = new Id3v2FrameHeader(FrameIdentifiers.WXXX);
-        header.frameSize = 12;
-        const data = ByteVector.concatenate(
-            header.render(4),
-            StringType.UTF16BE,
-            ByteVector.fromString("foo", StringType.UTF16BE),
-            ByteVector.getTextDelimiter(StringType.UTF16BE),
-            ByteVector.fromString("bar", StringType.Latin1)
-        );
-
-        // Act
-        const output = UrlLinkFrame.fromRawData(data, 4);
 
         // Assert
         Id3v2_UrlLinkFrame_ConstructorTests.assertFrame(
